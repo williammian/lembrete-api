aeeb6045baad8bb03dc20f589016fb0e3310755e
williammian <william_mian@yahoo.com.br>
2020-01-19 18:02:47 -0200

    API RESTful lembretes

---
+++ others/schema_base_de_dados.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE lembretes
+
+CREATE SCHEMA lembretes 
+
+CREATE TABLE lembretes.lembrete
+(
+   id bigserial NOT NULL PRIMARY KEY,
+   conteudo character varying(255) NOT NULL,
+   arquivado BOOLEAN,
+   prioridade character varying(20) CHECK ( prioridade in ('BAIXA', 'MEDIA', 'ALTA')) NOT NULL,
+   modificado INTEGER
+);
---
+++ src/main/java/br/com/wm/lembreteapi/LembreteApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.wm.lembreteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LembreteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LembreteApiApplication.class, args);
+	}
+
+}
